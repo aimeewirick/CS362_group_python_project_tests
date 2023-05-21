316ba73683120db0ae6a93a7580fd5b8e79e2056
Aimee Wirick <73860287+aimeewirick@users.noreply.github.com>
2023-05-21 12:22:11 -0700

    Created drafts of conv_num in task.py and tests for conv_num in tests.py

--- task.py
+++ task.py
@@ -1,3 +1,136 @@
 
 def conv_num(num_str):
-    pass
+    """Converts a number string to an integer or float including hexadecimal or base10 numbers"""
+    allowed_alphabet = "ABCDEFabcdef"
+    allowed_prefix = "xX"
+    prefix_options = ["0X", "0x"]
+    allowed_numbers = "1234567890"
+    allowed_sign = "-"
+    allowed_decimal = "."
+    all_allowed = allowed_alphabet + allowed_sign + allowed_prefix + allowed_decimal + allowed_numbers
+    number = None
+    hexa = False
+    negative = False
+    decimal = False
+    # Checks correct string type and basic content
+    if type(num_str) != type(allowed_numbers):
+        return number
+    if num_str == "":
+        return number
+    # Checks for negative sign
+    if allowed_sign in num_str:
+        if num_str.index(allowed_sign) != 0:
+            return number
+        negative = True
+    # Checks for correct hexadecimal prefix
+    for option in prefix_options:
+        if option in num_str:
+            if negative is True:
+                if num_str.index(option) == 1:
+                    hexa = True
+            if num_str.index(option) == 0:
+                hexa = True
+    string = []
+    # Cuts out the prefix so we don't need to check that part redundantly
+    if hexa is True:
+        string = num_str[2:len(num_str)]
+        if negative is True:
+            string = num_str[3:len(num_str)]
+    else:
+        if negative is True:
+            string = num_str[1:len(num_str)]
+        else:
+            string = num_str[0:len(num_str)]
+    # Checks the string content without negative or hexadecimal prefix
+    for item in string:
+        if item not in all_allowed:
+            return None
+        if item in allowed_prefix:
+            return number
+        if item in allowed_alphabet:
+            if hexa is False:
+                return number
+        if item in allowed_decimal:
+            if hexa is True:
+                return number
+            if decimal is True:
+                return number
+            decimal = True
+    if hexa is True:
+        # Converts a hexadecimal string
+        number = hex_converter(string, negative)
+    else:
+        # Converts a base 10 string
+        number = base10_converter(string, negative, decimal)
+    return number
+
+
+def hex_converter(string, negative):
+    """Helper function to convert a hexadecimal string to an integer"""
+    number = 0
+    exponent = len(string) - 1
+    converted_string = string_to_number(string)
+    for num in converted_string:
+        total = num * (16 ** exponent)
+        number = number + total
+        exponent -= 1
+    if negative is True:
+        number = number * -1
+    return number
+
+
+def string_to_number(string):
+    """Helper function to convert a number string (hexadecimal or decimal) to a list of numbers"""
+    number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    number_string = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    upper_letter_string = ["A", "B", "C", "D", "E", "F"]
+    lower_letter_string = ["a", "b", "c", "d", "e", "f"]
+    hex_number_list = [10, 11, 12, 13, 14, 15]
+    converted_string = []
+    for num in string:
+        # uses indexing to lookup number equivalents by their string counterpart
+        if num in number_string:
+            converted_string.append(number_list[number_string.index(num)])
+        if num in upper_letter_string:
+            converted_string.append(hex_number_list[upper_letter_string.index(num)])
+        if num in lower_letter_string:
+            converted_string.append(hex_number_list[lower_letter_string.index(num)])
+    return converted_string
+
+
+def base10_converter(string, negative, decimal):
+    """Helper function to convert a base10 number string (whole or decimal) to an integer or float"""
+    whole_numbers = []
+    decimal_numbers = []
+    new_string = string
+    place = len(new_string)
+    # Splits up the whole numbers and decimal numbers to separate strings for conversion into lists
+    if decimal is True:
+        place = new_string.index(".")
+        whole_numbers = string_to_number(new_string[0:place])
+        decimal_numbers = string_to_number(new_string[place+1:len(new_string)])
+    if decimal is False:
+        whole_numbers = string_to_number(new_string)
+    converted_number = 0
+    divisor = 10
+    if place == len(new_string) - 1:
+        converted_number = converted_number + .0
+    if whole_numbers:
+        # Converts numbers in a whole number list to a whole number
+        for num in whole_numbers:
+            total = num * 10 ** (place - 1)
+            converted_number = converted_number + total
+            place -= 1
+    if decimal_numbers:
+        # Converts numbers in decimal list to a decimal number
+        for num in decimal_numbers:
+            total = num / divisor
+            divisor = divisor * 10
+            converted_number = converted_number + total
+    if negative is True:
+        converted_number = converted_number * -1
+    if len(decimal_numbers) > 4:
+        converted_number = round(converted_number, len(decimal_numbers))
+    else:
+        converted_number = round(converted_number, 4)
+    return converted_number
--- tests.py
+++ tests.py
@@ -1,10 +1,160 @@
 import unittest
+from task import conv_num
+import random
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test1_conv(self):
+        # tests for random letters
+        value = "alkdjf;lakjde"
+        self.assertIsNone(conv_num(value))
+
+    def test2_conv(self):
+        # tests for correct number with decimal input
+        value = "123.45"
+        self.assertEqual(conv_num(value), 123.45)
+
+    def test2_5_conv(self):
+        # tests for correct negative number with decimal input
+        value = "-123.45"
+        self.assertEqual(conv_num(value), -123.45)
+
+    def test2_6_conv(self):
+        # tests for two decimals
+        value = "123.4.5"
+        self.assertIsNone(conv_num(value))
+
+    def test3_conv(self):
+        # tests for - in wrong place
+        value = "123-456"
+        self.assertIsNone(conv_num(value))
+
+    def test4_conv(self):
+        # tests for - in right place
+        value = "-1235"
+        self.assertEqual(conv_num(value), -1235)
+        print(type(conv_num(value)))
+
+    def test5_conv(self):
+        # checks if hex-decimal is correctly represented as 0x or 0X
+        value = "01230x345"
+        self.assertIsNone(conv_num(value))
+
+    def test6_conv(self):
+        # checks on a correct hex-decimal prefix
+        value = "0X12345"
+        self.assertEqual(conv_num(value), 74565)
+
+    def test7_conv(self):
+        # checks on a repeating correct hex-decimal prefix
+        value = "0x12340x"
+        self.assertIsNone(conv_num(value))
+
+    def test7_5_conv(self):
+        # checks on 'x' in incorrect location with '0' at beginning
+        value = "09x234"
+        self.assertIsNone(conv_num(value))
+
+    def test7_6_conv(self):
+        # checks negative with hex in wrong place
+        value = "-056x890"
+        self.assertIsNone(conv_num(value))
+
+    def test8_conv(self):
+        # checks on an alphabetical string with no hex-decimal prefix
+        value = "abcdef"
+        self.assertIsNone(conv_num(value))
+
+    def test9_conv(self):
+        # checks correct negative hex-decimal
+        value = "-0xa456"
+        self.assertEqual(conv_num(value), -42070)
+
+    def test10_conv(self):
+        # checks decimal with hex-decimal prefix
+        value = "0x23.45"
+        self.assertIsNone(conv_num(value))
+
+    def test11_conv(self):
+        # tests an empty string
+        value = ""
+        self.assertIsNone(conv_num(value))
+
+    def test12_conv(self):
+        # tests a input that is not a string
+        value = 12345
+        self.assertIsNone(conv_num(value))
+
+    def test13_conv(self):
+        # checks the hex_decimal converter
+        value = "0x7CF"
+        self.assertTrue(conv_num(value))
+
+    def test14_conv(self):
+        # checks a negative hex_decimal with conversion
+        value = "-0x7CF"
+        self.assertEqual(conv_num(value), -1999)
+
+    def test15_conv(self):
+        # checks number with a decimal in the beginning
+        value = ".45"
+        self.assertEqual(conv_num(value), 0.45)
+
+    def test16_conv(self):
+        # checks number with decimal at the end
+        value = "123."
+        self.assertEqual(conv_num(value), 123.0)
+        print(conv_num(value))
+
+    def test17_conv(self):
+        # checks edge case number with decimal one space away from end
+        value = "123.0"
+        self.assertEqual(conv_num(value), 123.0)
+        print(type(value))
+        print(type(conv_num(value)))
+
+    def test18_conv(self):
+        # checks for items that are not in allowed inputs
+        value = [1]
+        self.assertIsNone(conv_num(value))
+
+    def test19_conv(self):
+        test_num = 3000
+        # checks for correct hex numbers
+        while test_num > 0:
+            rand_num = random.randint(1, 20000)
+            hex_num = hex(rand_num)
+            value = str(hex_num)
+            print(value)
+            self.assertEqual(conv_num(value), rand_num)
+            test_num -= 1
+
+    def test20_conv(self):
+        # test for correct integers
+        test_num = 3000
+        while test_num > 0:
+            rand_num = random.randint(-10000, 10000)
+            value = str(rand_num)
+            self.assertEqual(conv_num(value), rand_num)
+            print(conv_num(value))
+            test_num -= 1
+
+    def test21_conv(self):
+        # test for correct float numbers
+        test_num = 3000
+        while test_num > 0:
+            rand_num = random.randint(-10000, 10000)
+            rand_decimal = random.randint(0, 10000)
+            str_decimal = str(rand_decimal)
+            dec_point = len(str_decimal)
+            decimal = rand_decimal / (10 ** dec_point)
+            number = rand_num + decimal
+            value = str(number)
+            self.assertAlmostEqual(conv_num(value), number)
+            print(number, "number")
+            print(conv_num(value), "converted number")
+            test_num -= 1
 
 
 if __name__ == '__main__':
