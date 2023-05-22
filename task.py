10633b8ea4b268dc4bf7cef0e53d5027efeb461a
Drew Niman <drewmniman@gmail.com>
2023-05-22 09:19:08 -0700

    added conv_end function

--- task.py
+++ task.py
@@ -1,3 +1,15 @@
 
 def conv_num(num_str):
     pass
+
+
+def conv_endian(num, endian='big'):
+    '''
+    - Takes in an integer (+/-) value as num and
+    converts it to a hexadecimal number.
+    - The endian type is determined by the flag endian.
+    - Returns the converted number as a string,
+    or None in case of invalid endian type.
+    - Format = "XX YY ZZ"
+    '''
+    pass
