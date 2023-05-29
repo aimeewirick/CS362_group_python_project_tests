1b3807e4cb991b1dcc7930e8fa7b35819f2f0db7
ValorNash <valornash@gmail.com>
2023-05-28 17:42:10 -0700

    Finalized Datetime and finished unit tests.

--- task.py
+++ task.py
@@ -3,4 +3,63 @@ def conv_num(num_str):
 
 
 def my_datetime(num_sec):
-    pass
+    # Constants
+    sec_per_day = 86400
+    sec_per_year = 365 * sec_per_day
+
+
+    # Days per month
+    days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # Calculate number of years...
+    years = num_sec // sec_per_year
+    secs_left = num_sec - (sec_per_year * years)
+
+    # Helper function to check number of leap years present
+    def leap_year(year):
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    return True
+                else:
+                    return False
+            else:
+                return True
+        else:
+            return False
+
+    # Calculate number of leap years
+    leap_years = 0
+    for year in range(1970, 1970 + years):
+        if leap_year(year):
+            leap_years += 1
+
+    # Calculate seconds left after removing the years that have occurred w/ leap years:
+    secs_left = secs_left - (sec_per_day * leap_years)
+
+    # Adjust February if current year is a leap year
+    if leap_year(1970 + years):
+        days_per_month[1] = 29
+
+    # Calculate days that have occurred in current year
+    days = (secs_left // sec_per_day)
+
+    # Calculate months
+    month = 0
+    while month < 12 and days >= days_per_month[month]:
+        days -= days_per_month[month]
+        month += 1
+
+    # Date components and formatting
+    day = days + 1
+    month += 1
+    year = 1970 + years
+
+    date_string = "{:02d}-{:02d}-{:04d}".format(month, day, year)
+
+    return date_string
+
+
+
+
+
--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 import unittest
+import datetime
+
 from task import my_datetime
+
 
 
 class TestCase(unittest.TestCase):
@@ -9,7 +12,54 @@ class TestCase(unittest.TestCase):
 
     def test1_date(self):
         # Tests the epoch
-        self.assertEqual(my_datetime(0), "01-01-1970")
+        seconds = 0
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test2_date(self):
+        # Second test from example, a date in the past
+        seconds = 123456789
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test3_date(self):
+        # Testing future date
+        seconds = 2000000000
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test4_date(self):
+        # Testing a leap year
+        seconds = 94694400
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test5_date(self):
+        # Testing a non-leap year
+        seconds = 31536000
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test6_date(self):
+        # A year that's usually a leap year, but is not because it's not divisible by 400
+        seconds = 4102444800
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+    def test7_date(self):
+        # Testing a time far in the future
+        seconds = 4158931200
+        expected_date = datetime.datetime.utcfromtimestamp(seconds)
+        formatted_date = expected_date.strftime('%m-%d-%Y')
+        self.assertEqual(formatted_date, my_datetime(seconds))
+
+
 
 
 if __name__ == '__main__':
